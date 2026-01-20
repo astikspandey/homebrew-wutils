15d12562d936391a383fa2d56970ae8fb3098756
astikspandey <astikspandey@gmail.com>
2026-01-20 11:02:06 +0530

    Install all formulas regardless of current state

--- install.sh
+++ install.sh
@@ -2,7 +2,8 @@
 brew tap astikspandey/wutils
 for f in $(ls $(brew --repo astikspandey/wutils)/Formula | sed 's/\.rb$//'); do
   [ "$f" = "wutils-install" ] && continue
-  brew list --formula | grep -qx "$f" || { echo "Installing $f..."; brew install astikspandey/wutils/$f; }
+  echo "Installing $f..."
+  brew install astikspandey/wutils/$f
 done
 echo "Removing wutils-install..."
 brew uninstall wutils-install
