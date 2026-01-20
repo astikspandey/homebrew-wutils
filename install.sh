387ee9e84571ced633c3ee811143d802158e60bb
astikspandey <astikspandey@gmail.com>
2026-01-20 11:10:41 +0530

    Add verbose option to wutils-install

--- install.sh
+++ install.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
+VERBOSE=""
+if [ "$1" = "-v" ] || [ "$1" = "--verbose" ]; then
+  VERBOSE="--verbose"
+fi
+
 brew tap astikspandey/wutils
 for f in $(ls $(brew --repo astikspandey/wutils)/Formula | sed 's/\.rb$//'); do
   [ "$f" = "wutils-install" ] && continue
   echo "Installing $f..."
-  brew install astikspandey/wutils/$f
+  brew install $VERBOSE astikspandey/wutils/$f
 done
 echo "Removing wutils-install..."
 brew uninstall wutils-install
