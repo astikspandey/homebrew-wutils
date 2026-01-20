6fa0f7f496229ce64c740a6921218e5aa16c30d8
astikspandey <astikspandey@gmail.com>
2026-01-20 11:11:50 +0530

    Update SHA256

--- Formula/wutils-filegen.rb
+++ Formula/wutils-filegen.rb
@@ -2,7 +2,7 @@ class WutilsFilegen < Formula
   desc "Generate files of specified size"
   homepage "https://github.com/astikspandey/homebrew-wutils"
   url "https://github.com/astikspandey/homebrew-wutils/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "6eeac18a41ba9a93dd7045a289e4c90e7bd692027ee5e606022a7b146485dd6c"
+  sha256 "2be3070f4da298407895e883e27edcda5b82c2dbf98ca2228db9eec0b501da65"
   license "MIT"
   version "1.0.0"
 
