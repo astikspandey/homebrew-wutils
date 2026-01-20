23f1a327f2d0a3a768e7a72800facd40b4f60976
astikspandey <astikspandey@gmail.com>
2026-01-20 10:59:27 +0530

    Make wutils-install delete itself after running

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 brew tap astikspandey/wutils
 for f in $(ls $(brew --repo astikspandey/wutils)/Formula | sed 's/\.rb$//'); do
+  [ "$f" = "wutils-install" ] && continue
   brew list --formula | grep -qx "$f" || { echo "Installing $f..."; brew install astikspandey/wutils/$f; }
 done
+echo "Removing wutils-install..."
+brew uninstall wutils-install
