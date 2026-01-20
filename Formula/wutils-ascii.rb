5db7853045c4ad048f899b17c771d4127b95f3ef
astikspandey <astikspandey@gmail.com>
2026-01-20 10:34:29 +0530

    Update SHA256

--- Formula/wutils-ascii.rb
+++ Formula/wutils-ascii.rb
@@ -2,7 +2,7 @@ class WutilsAscii < Formula
   desc "ASCII art text generator using 5x7 font"
   homepage "https://github.com/astikspandey/homebrew-wutils"
   url "https://github.com/astikspandey/homebrew-wutils/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "3ec3bc0a75e7042449a8e5f0382127b0405098c094e638c8da43df58c6a61167"
+  sha256 "89f257c250310a9cd21be9b22e9d21a0fdad576f75050fd9e425f6932a815bda"
   license "MIT"
   version "1.0.0"
 
