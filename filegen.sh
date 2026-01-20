0d013d96c0755fb65d2b7319a14f334828e12a15
astikspandey <astikspandey@gmail.com>
2026-01-20 10:37:39 +0530

    Fix usage message

--- filegen.sh
+++ filegen.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ -z "$1" ]; then
-  echo "Usage: $0 <file size in kb> <output file (optional)>"
+  echo "Usage: filegen <file size in kb> <output file (optional)>"
 elif [ -z $2 ]; then
   curl -s "https://file-gen.vercel.app?kb=$1"
 else
