3ff90e1e5df66a0332976a866b4e19e13fb3f766
astikspandey <astikspandey@gmail.com>
2026-01-20 10:31:43 +0530

    Add shebang to filegen

--- filegen.sh
+++ filegen.sh
@@ -1,4 +1,5 @@
-if [ -z "$1" ]; then	
+#!/bin/bash
+if [ -z "$1" ]; then
   echo "Usage: $0 <file size in kb> <output file (optional)>"
 elif [ -z $2 ]; then
   curl -s "https://file-gen.vercel.app?kb=$1"
